276ee6c8000c284b47fe4b0a699088eb83926c13
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-09-07 15:11:41 -0400

    Setup Tailwind CSS and ESlint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind CSS with React and TypeScript!
+      </h1>
+    </div>
+  )
+}
+
+export default App
